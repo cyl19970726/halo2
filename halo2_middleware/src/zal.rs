53fb0185bc05ea818d5f4f8b3c2b670520cfa8fa
cyl19970726 <15258378443@163.com>
2024-03-12 15:32:15 +0800

    add icicle implement for zal trait

--- halo2_middleware/src/zal.rs
+++ halo2_middleware/src/zal.rs
@@ -74,8 +74,10 @@ pub mod traits {
         // - Converting from Montgomery to Canonical form
         // - Input changed from Projective to Jacobian coordinates or even to a Twisted Edwards curve.
         // - other form of expensive preprocessing
-        type CoeffsDescriptor<'c>: Copy;
-        type BaseDescriptor<'b>: Copy;
+        // type CoeffsDescriptor<'c>: Copy;
+        // type BaseDescriptor<'b>: Copy;
+        type CoeffsDescriptor<'c>;
+        type BaseDescriptor<'b>;
 
         fn get_coeffs_descriptor<'c>(&self, coeffs: &'c [C::Scalar]) -> Self::CoeffsDescriptor<'c>;
         fn get_base_descriptor<'b>(&self, base: &'b [C]) -> Self::BaseDescriptor<'b>;
@@ -244,6 +246,269 @@ pub mod impls {
     }
 }
 
+pub mod gpu_impls {
+    use std::marker::PhantomData;
+
+    use crate::zal::traits::MsmAccel;
+    // use halo2curves::msm::best_multiexp;
+    use ff::PrimeField as Halo2FieldTrait;
+    use halo2curves::bn256::{Fq as Halo2BaseFiled, Fr as Halo2ScalarField, G1Affine, G1};
+    use halo2curves::CurveAffine as Halo2CurveAffine;
+    use icicle_bn254::curve::{
+        BaseField as IcicleBn254CurveBaseField, CurveCfg as IcicleBn254Curve,
+        G1Affine as IcicleBn254G1Affine, G1Projective,
+        ScalarCfg as IcicleBn254ScalarCfg, ScalarField as IcicleBn254CurveScalarField,
+    };
+    use icicle_core::traits::FieldImpl as IcicleFieldTrait;
+    use icicle_core::{
+        curve::Affine as IcicleCurveAffine, curve::Curve as IcicleCurve,
+        field::Field as IcicleField, msm as icicle_msm, traits::GenerateRandom,
+    };
+    use icicle_cuda_runtime::{memory::HostOrDeviceSlice, stream::CudaStream};
+
+    pub trait CurveAffineConversion {
+        type OriginCurveAffine;
+        type OriginCurveScalar: IcicleFieldTrait;
+        type TargetCurveAffine: Halo2CurveAffine;
+        type TargetCurveField: Halo2FieldTrait;
+        fn to_target_curve_affine(element: Self::OriginCurveAffine) -> Self::TargetCurveAffine;
+        fn to_target_curve_affines(
+            elements: &[Self::OriginCurveAffine],
+        ) -> Vec<Self::TargetCurveAffine>;
+        fn to_origin_curve_affine(element: Self::TargetCurveAffine) -> Self::OriginCurveAffine;
+        fn to_origin_curve_affines(
+            elements: &[Self::TargetCurveAffine],
+        ) -> Vec<Self::OriginCurveAffine>;
+        fn to_target_curve_scalar(element: Self::OriginCurveScalar) -> Self::TargetCurveField;
+        fn to_origin_curve_scalar(element: Self::TargetCurveField) -> Self::OriginCurveScalar;
+        fn to_origin_curve_scalars(
+            elements: &[Self::TargetCurveField],
+        ) -> Vec<Self::OriginCurveScalar>;
+    }
+
+    // Halo2curve Backend
+    // ---------------------------------------------------
+    pub struct GpuEngine<'a> {
+        msm_cfg: icicle_msm::MSMConfig<'a>,
+        // stream: CudaStream,
+    }
+
+    impl<'a> CurveAffineConversion for GpuEngine<'a> {
+        type OriginCurveAffine = IcicleBn254G1Affine;
+        type OriginCurveScalar = IcicleBn254CurveScalarField;
+        type TargetCurveAffine = G1Affine;
+        type TargetCurveField = Halo2ScalarField;
+        fn to_target_curve_affine(element: IcicleBn254G1Affine) -> Self::TargetCurveAffine {
+            let origin_x: Vec<u8> = element.x.to_bytes_le();
+            let origin_y: Vec<u8> = element.y.to_bytes_le();
+
+            let target_x = Halo2BaseFiled::from_bytes(&origin_x.try_into().unwrap()).unwrap();
+            let target_y = Halo2BaseFiled::from_bytes(&origin_y.try_into().unwrap()).unwrap();
+            G1Affine::from_xy(target_x, target_y).unwrap()
+        }
+
+        fn to_target_curve_affines(
+            elements: &[Self::OriginCurveAffine],
+        ) -> Vec<Self::TargetCurveAffine> {
+            let mut target_affines = Vec::new();
+            for element in elements.iter() {
+                target_affines.push(Self::to_target_curve_affine(*element));
+            }
+            target_affines
+        }
+
+        fn to_origin_curve_affines(
+            elements: &[Self::TargetCurveAffine],
+        ) -> Vec<Self::OriginCurveAffine> {
+            let mut origin_affines = Vec::new();
+            for element in elements.iter() {
+                origin_affines.push(Self::to_origin_curve_affine(*element));
+            }
+            origin_affines
+        }
+
+        fn to_origin_curve_affine(element: G1Affine) -> IcicleBn254G1Affine {
+            let origin_x: [u8; 32] = element.x.to_bytes();
+            let origin_y: [u8; 32] = element.y.to_bytes();
+
+            let target_x = IcicleBn254CurveBaseField::from_bytes_le(&origin_x);
+            let target_y = IcicleBn254CurveBaseField::from_bytes_le(&origin_y);
+            IcicleBn254G1Affine::from_limbs(target_x.into(), target_y.into())
+        }
+
+        fn to_target_curve_scalar(element: IcicleBn254CurveScalarField) -> Halo2ScalarField {
+            let origin_scalar = element.to_bytes_le();
+            let target_scalar =
+                Halo2ScalarField::from_bytes(&origin_scalar.try_into().unwrap()).unwrap();
+            target_scalar
+        }
+
+        fn to_origin_curve_scalar(element: Halo2ScalarField) -> IcicleBn254CurveScalarField {
+            let origin_scalar = element.to_bytes();
+            let target_scalar = IcicleBn254CurveScalarField::from_bytes_le(&origin_scalar);
+            target_scalar
+        }
+
+        fn to_origin_curve_scalars(
+            elements: &[Halo2ScalarField],
+        ) -> Vec<IcicleBn254CurveScalarField> {
+            let mut origin_scalars = Vec::new();
+            for element in elements.iter() {
+                origin_scalars.push(Self::to_origin_curve_scalar(*element));
+            }
+            origin_scalars
+        }
+    }
+
+    pub struct GpuMsmCoeffsDesc<'c> {
+        raw: HostOrDeviceSlice<'c, IcicleBn254CurveScalarField>,
+    }
+
+    pub struct GpuEngineMsmBaseDesc<'b> {
+        raw: HostOrDeviceSlice<'b, IcicleBn254G1Affine>,
+    }
+
+    impl<'a> GpuEngine<'a> {
+        pub fn new() -> (Self,CudaStream) {
+            let stream = CudaStream::create().unwrap();
+            let mut cfg = icicle_msm::MSMConfig::default();
+            // cfg.ctx.stream = &stream;
+            cfg.is_async = true;
+            (Self { 
+                msm_cfg: cfg,
+            },
+            stream)
+        }
+    }
+
+    impl<'a> MsmAccel<G1Affine> for GpuEngine<'a> {
+        fn msm(&self, coeffs: &[Halo2ScalarField], bases: &[G1Affine]) -> G1 {
+            let mut msm_results: HostOrDeviceSlice<'_, G1Projective> =
+                HostOrDeviceSlice::cuda_malloc(1).unwrap();
+            let mut msm_host_result = vec![G1Projective::zero(); 1];
+
+            let coeffs_host = Self::to_origin_curve_scalars(coeffs);
+            let coeffs_host = HostOrDeviceSlice::Host(coeffs_host);
+
+            let bases_host = Self::to_origin_curve_affines(bases);
+            let bases_host = HostOrDeviceSlice::Host(bases_host);
+
+            icicle_msm::msm(&coeffs_host, &bases_host, &self.msm_cfg, &mut msm_results);
+            println!("Executing bn254 MSM on device...");
+
+            msm_results.copy_to_host(&mut msm_host_result[..]).unwrap();
+
+            let result = IcicleBn254G1Affine::from(msm_host_result[0]);
+            let result = Self::to_target_curve_affine(result);
+            result.into()
+        }
+
+        // Caching API
+        // -------------------------------------------------
+
+        type CoeffsDescriptor<'c> = GpuMsmCoeffsDesc<'c>;
+        type BaseDescriptor<'b> = GpuEngineMsmBaseDesc<'b>;
+
+        fn get_coeffs_descriptor<'c>(
+            &self,
+            coeffs: &'c [Halo2ScalarField],
+        ) -> Self::CoeffsDescriptor<'c> {
+            // Do expensive device/library specific preprocessing here
+            // Todo: consider that the life time of coeffs_host
+            let coeffs_host = Self::to_origin_curve_scalars(coeffs);
+            let mut coeffs_device: HostOrDeviceSlice<'c, IcicleBn254CurveScalarField> =
+                HostOrDeviceSlice::cuda_malloc(coeffs_host.len()).unwrap();
+            coeffs_device.copy_from_host(&coeffs_host).unwrap();
+            Self::CoeffsDescriptor { raw: coeffs_device }
+        }
+
+        fn get_base_descriptor<'b>(&self, bases: &'b [G1Affine]) -> Self::BaseDescriptor<'b> {
+            let bases_host = Self::to_origin_curve_affines(bases);
+            let mut bases_device: HostOrDeviceSlice<'b, IcicleBn254G1Affine> =
+                HostOrDeviceSlice::cuda_malloc(bases_host.len()).unwrap();
+            bases_device.copy_from_host(&bases_host).unwrap();
+            Self::BaseDescriptor { raw: bases_device }
+        }
+
+        fn msm_with_cached_scalars(
+            &self,
+            coeffs: &Self::CoeffsDescriptor<'_>,
+            bases: &[G1Affine],
+        ) -> G1 {
+            let mut msm_device_results: HostOrDeviceSlice<'_, G1Projective> =
+                HostOrDeviceSlice::cuda_malloc(1).unwrap();
+            let mut msm_host_result = vec![G1Projective::zero(); 1];
+
+            let bases_host = Self::to_origin_curve_affines(bases);
+            let bases_host = HostOrDeviceSlice::Host(bases_host);
+
+            icicle_msm::msm(
+                &coeffs.raw,
+                &bases_host,
+                &self.msm_cfg,
+                &mut msm_device_results,
+            );
+
+            msm_device_results
+                .copy_to_host(&mut msm_host_result[..])
+                .unwrap();
+            let result = IcicleBn254G1Affine::from(msm_host_result[0]);
+            let result = Self::to_target_curve_affine(result);
+            result.into()
+        }
+
+        fn msm_with_cached_base(
+            &self,
+            coeffs: &[Halo2ScalarField],
+            bases: &Self::BaseDescriptor<'_>,
+        ) -> G1 {
+            let mut msm_device_results: HostOrDeviceSlice<'_, G1Projective> =
+                HostOrDeviceSlice::cuda_malloc(1).unwrap();
+            let mut msm_host_result = vec![G1Projective::zero(); 1];
+
+            let coeffs_host = Self::to_origin_curve_scalars(coeffs);
+            let coeffs_host = HostOrDeviceSlice::Host(coeffs_host);
+
+            icicle_msm::msm(
+                &coeffs_host,
+                &bases.raw,
+                &self.msm_cfg,
+                &mut msm_device_results,
+            );
+
+            msm_device_results
+                .copy_to_host(&mut msm_host_result[..])
+                .unwrap();
+            let result = IcicleBn254G1Affine::from(msm_host_result[0]);
+            let result = Self::to_target_curve_affine(result);
+            result.into()
+        }
+
+        fn msm_with_cached_inputs(
+            &self,
+            coeffs: &Self::CoeffsDescriptor<'_>,
+            bases: &Self::BaseDescriptor<'_>,
+        ) -> G1 {
+            let mut msm_device_results: HostOrDeviceSlice<'_, G1Projective> =
+                HostOrDeviceSlice::cuda_malloc(1).unwrap();
+            let mut msm_host_result = vec![G1Projective::zero(); 1];
+
+            icicle_msm::msm(
+                &coeffs.raw,
+                &bases.raw,
+                &self.msm_cfg,
+                &mut msm_device_results,
+            );
+
+            msm_device_results
+                .copy_to_host(&mut msm_host_result[..])
+                .unwrap();
+            let result = IcicleBn254G1Affine::from(msm_host_result[0]);
+            let result = Self::to_target_curve_affine(result);
+            result.into()
+        }
+    }
+}
 // Testing
 // ---------------------------------------------------
 
